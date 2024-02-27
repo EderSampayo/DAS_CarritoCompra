da46529b9eb2393a33962207e4591b9538ecbecc
EderSampayo <edersampayoupv@gmail.com>
2024-02-27 13:59:15 +0100

    Interfaz usuario con navbar

--- app/src/main/java/com/example/das_carritocompra/ActividadUsuario.java
+++ app/src/main/java/com/example/das_carritocompra/ActividadUsuario.java
@@ -1,7 +1,47 @@
 package com.example.das_carritocompra;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class ActividadUsuario extends AppCompatActivity {
 
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.actividad_usuario);
+
+        Toolbar toolbar = findViewById(R.id.labarra);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.definicion_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemId = item.getItemId();
+
+        if (itemId == R.id.menu_carrito) {
+            startActivity(new Intent(this, MainActivity.class));
+            return true;
+        } else if (itemId == R.id.menu_productos) {
+            startActivity(new Intent(this, ActividadProductos.class));
+            return true;
+        } else if (itemId == R.id.menu_usuario) {
+            // No hace nada, ya estamos en la actividad de usuario
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
+    }
 }
